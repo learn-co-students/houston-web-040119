56488a380f01aaeac452ea8d41c51f8ea3b6d879
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-05-22 15:10:17 -0500

    sorted

--- 30-TaskLister-review/src/taskList.js
+++ 30-TaskLister-review/src/taskList.js
@@ -1,26 +1,26 @@
 class TaskList {
 
-    constructor(){
-        this.ul = document.createElement('ul')
-        this.h2 = document.createElement('h2')
-        this.h2.innerText = "TO-DO"
-    }
-
-
-  render(desc){
-     let taskitem = new TaskListItem(desc)
-     let liArray = taskitem.renderListItem()
-     liArray.forEach((li) => {
-         this.ul.append(li)
-     })
+  constructor(){
+      this.ul = document.createElement('ul')
+      this.h2 = document.createElement('h2')
+      this.h2.innerText = "TO-DO"
   }
 
-  sorting(sort){
-    let liArray = TaskListItem.sortListItem(sort)
-    liArray.forEach((li) => {
-        this.ul.append(li)
-    })
-  }
+
+render(desc){
+   let taskitem = new TaskListItem(desc)
+   let liArray = taskitem.renderListItem()
+   liArray.forEach((li) => {
+       this.ul.append(li)
+   })
+}
+
+sorting(sort){
+  let liArray = TaskListItem.sortListItem(sort)
+  liArray.forEach((li) => {
+      this.ul.append(li)
+  })
+}
 
 
 }
