a6d3a7f00ead5b6e5c2280f2499091ab0dc36b13
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-06-06 10:10:44 -0500

    initial commit

---
+++ 37-Component-LifeCycle/src/Child.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+class Child extends Component{
+    render(){
+
+    }
+}
+
+export default Child
---
+++ 37-Component-LifeCycle/src/Parent.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+class Parent extends Component{
+    render(){
+
+    }
+}
+
+export default Parent
